ad0ed5561595673e70891e2e8802a3bf8bfefc54
erdxd <tytalexsey@mail.ru>
2025-08-13 20:30:33 +0700

    Add Task1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # taskongolang
+This repository have tasks on language golang which often used on interview.
+I will try append new tasks in this repository every weeks or sooner
---
+++ task1.go
@@ -0,0 +1,7 @@
+package main //Task 1 -  To understand how the built-in max and min function works || https://t.me/golangtask/7
+import "fmt" // green lvl
+
+func main() {
+	fmt.Println(min("long", "Short"))
+
+}
