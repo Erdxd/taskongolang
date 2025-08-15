da44f294865a8d1d1fb8554b2786040d236fd442
erdxd <tytalexsey@mail.ru>
2025-08-15 17:54:15 +0700

    Add new task

---
+++ Task4/Task4.go
@@ -0,0 +1,11 @@
+package Task4
+
+import "fmt"
+
+func Task4() {
+	fmt.Print("A")
+	switch i := 17; i%2 == 0 {
+	case i%2 == 0:
+	}
+	fmt.Println("B")
+}
--- checkresult.go
+++ checkresult.go
@@ -5,25 +5,56 @@ import (
 	"myproject/Task1"
 	"myproject/Task2"
 	"myproject/Task3"
+	"myproject/Task4"
+
+	"github.com/fatih/color"
 )
 
 func main() {
+	defer color.Red("Exit")
+	defer color.Green("This app will be updated weekly or more frequently. Check your version at https://github.com/Erdxd/taskongolang.")
+
+	var menu = map[string]func(){
+		"1": Task1.Task1,
+		"2": Task2.Task2,
+		"3": Task3.Task3,
+		"4": Task4.Task4,
+	}
+menu:
 	for {
-		fmt.Println("Enter the number of the problem you want to get an answer to.(1-3)")
-		var User string
-		fmt.Scan(&User)
-		switch User {
-		case "1":
-			Task1.Task1()
-		case "2":
-			Task2.Task2()
-		case "3":
-			Task3.Task3()
-		default:
-			break
+		Useranswer := Enter([]string{
+			"1-Task 1",
+			"2-Task 2",
+			"3-Task 3",
+			"4-Task 4",
+			"0 - close",
+			"Выберите вариант",
+		})
+		FuncMenu := menu[Useranswer]
+		if Useranswer == "0" {
+
+			break menu
+		} else if FuncMenu == nil {
+			color.Yellow("Error, please repeat attempt")
+			continue
 		}
+
+		FuncMenu()
+
 	}
 }
-func Enter() {
+func Enter[T any](prompt []T) string {
+
+	for i, line := range prompt {
+		if i == len(prompt)-1 {
+			fmt.Printf("%v:", line)
+		} else {
+			fmt.Println(line)
+		}
+	}
+
+	var res string
+	fmt.Scanln(&res)
+	return res
 
 }
